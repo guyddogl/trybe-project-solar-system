e1edb9bb2e3ddc871536535cdf9828c61f141e6e
Guyddo <guyddogl@gmail.com>
2022-07-20 15:56:24 -0300

    Create Missions component

---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="missions">
+      </div>
+    );
+  }
+}
+
+export default Missions;
